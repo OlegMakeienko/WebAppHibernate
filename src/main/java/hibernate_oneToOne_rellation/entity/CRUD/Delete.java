9c8e9b45be416b90df38d50bdc858959ee6b7c2f
Oleg Makeienko <molva83@gmail.com>
2024-01-28 20:03:24 +0100

    created Delete but is not work

---
+++ src/main/java/hibernate_oneToOne_rellation/entity/CRUD/Delete.java
@@ -0,0 +1,34 @@
+package hibernate_oneToOne_rellation.entity.CRUD;
+
+import hibernate_oneToOne_rellation.entity.Detail;
+import hibernate_oneToOne_rellation.entity.Employees;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Delete {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("/hibernate.cfg.xml")
+                .addAnnotatedClass(Employees.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employees emp = session.get(Employees.class,1);
+            session.delete(emp);
+            System.out.println(emp);
+
+
+            System.out.println("Done!");
+        } finally {
+            assert session != null;
+            session.close();
+            factory.close();
+        }
+    }
+}
